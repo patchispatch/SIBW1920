b3de69e44b9f174bd0bb13e1803f4b0153e2e3cd
patchispatch <juanov98@gmail.com>
2020-03-19 18:58:34 +0100

    Añadidos comentarios y abre y cierre

---
+++ practica1/comentarios.js
@@ -0,0 +1,11 @@
+// Sección de comentarios
+
+/* Set the width of the sidebar to 250px (show it) */
+function openNav() {
+    document.getElementById("comments-section").style.display = "block";
+}
+
+/* Set the width of the sidebar to 0 (hide it) */
+function closeNav() {
+    document.getElementById("comments-section").style.display = "none";
+} 
