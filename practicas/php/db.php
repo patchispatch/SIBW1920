7d46626bde1baa1ac2e75758220d82a2c16b88c6
patchispatch <juanov98@gmail.com>
2020-04-01 19:25:47 +0200

    Arreglada conexión con BD

--- practicas/php/db.php
+++ practicas/php/db.php
@@ -2,10 +2,10 @@
 
 // Connects to the database
 function connect() {
-    $connection = new mysqli('localhost', 'pepe', 'mango', 'SIBW');
+    $connection = new mysqli("mysql", "pepe", "mango", "SIBW");
 
-    if(mysqli_connecto_errno()) {
-        die("Error de conexión");
+    if($connection->connect_errno) {
+        echo("Fallo al conectar: " . $connection->connect_error);
     }
 
     return $connection;
@@ -20,6 +20,24 @@ function query($connection, $query) {
     $connection->close();
 
     return $result;
+}
+
+// Returns event data in an associative array
+function event_info($id) {
+    // Connect to the database
+    $conn = connect();
+    // Prepare query
+    $stmt = "SELECT * FROM eventos WHERE id=" . $id;
+
+    // Send query and close connection
+    $result = query($conn, $stmt);
+
+    // Parse result:
+    if($result->num_rows > 0) {
+        $row = $result->fetch_assoc();
+    }
+
+    return $row;
 }
 
 
--- practicas/php/evento.php
+++ practicas/php/evento.php
@@ -1,14 +1,17 @@
 <?php
     require_once "/usr/local/lib/php/vendor/autoload.php";
+    require 'db.php';
 
     $loader = new \Twig\Loader\FilesystemLoader('../html');
     $twig = new \Twig\Environment($loader);
 
     // Page name
-    $title = "AAAAAAAAAH";
-    $author = "";
-    $date = "";
-    $content = "";
+    $event = event_info(1);
+
+    $title = $event["titulo"];
+    $author = $event["autor"];
+    $date = $event["fecha"];
+    $content = $event["texto"];
 
     // Render
     echo $twig->render('evento.html', [
